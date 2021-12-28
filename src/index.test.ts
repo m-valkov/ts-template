b63bdab29fb9b68c88079f0beff5245b32049872
Mikhail Valkov <mikhail.valkov@gmail.com>
2021-12-29 00:26:24 +0300

    save

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { Sum } from './index';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(Sum(1, 2)).toBe(3);
+});
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const Sum = (a: number, b: number): number => {
+  return a + b;
+};
